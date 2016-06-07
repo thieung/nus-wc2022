930fde9a2dfd1b93fa022bfe5730cb6a8f2bb157
Thieu Nguyen <thieunguyen.it@gmail.com>
2016-06-07 14:22:23 +0700

    Fix fonts issue on Heroku

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Euro2016
     config.i18n.default_locale = :vi
 
     config.assets.paths << Rails.root.join('lib', 'assets', 'fonts')
-    config.assets.precompile += %w(.svg .eot .woff .ttf)
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
