26aface0e4757f916e69c70e348efbe58a452d1c
Thieu Nguyen <thieunguyen.it@gmail.com>
2016-06-09 03:28:39 +0700

    Update config for test

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => Settings.sendgrid.username,
+  :password       => Settings.sendgrid.password,
+  :domain         => 'demo-euro2016.herokuapp.com',
+  :enable_starttls_auto => true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,15 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => Settings.sendgrid.username,
-    :password       => Settings.sendgrid.password,
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
-
   config.action_mailer.default_url_options = { host: 'nus-euro2016.herokuapp.com' }
 end
