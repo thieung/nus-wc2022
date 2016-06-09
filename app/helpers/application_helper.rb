f70d396b9f32b32eac5993fa07f611b884f7662d
Thieu Nguyen <thieunguyen.it@gmail.com>
2016-06-09 18:00:24 +0700

    Change text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   include ActionView::Helpers::NumberHelper
   def format_currency number, currency_text=true
     result = number_with_delimiter(number, delimiter: '.')
-    result+=" VNĐ" if currency_text
+    result+=" Points" if currency_text
     result
   end
 end
