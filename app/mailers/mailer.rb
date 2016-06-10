8b52df7557c068388559feff013867dff592e968
Thieu Nguyen <thieunguyen.it@gmail.com>
2016-06-10 09:32:02 +0700

    Update

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,12 +1,12 @@
 class Mailer
   def self.process_notice_bet_result_to_staffs bet_info
-    Settings.group_mails_with_hr.each do |recipient|
+    Settings.group_mails.each do |recipient|
       UserMailer.delay.notice_bet_result_to_staffs recipient, bet_info
     end
   end
 
   def self.process_notice_bet_result_updated_to_staffs bet_info
-    Settings.group_mails_with_hr.each do |recipient|
+    Settings.group_mails.each do |recipient|
       UserMailer.delay.notice_bet_result_updated_to_staffs recipient, bet_info
     end
   end
