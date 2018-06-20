df3682c0d2a6f54527be8750e23fe3a4e849992c
Thieu Nguyen <thieunguyen.it@gmail.com>
2018-06-20 18:10:15 +0700

    Update assets compile setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'nus-wc2018.herokuapp.com' }
-
-  config.assets.compile = false
 end
