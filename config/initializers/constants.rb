b80cdd0fc3eb463f173c1259a246c87758173dc6
Thieu Nguyen <thieunguyen.it@gmail.com>
2018-07-16 03:47:20 +0700

    Update songs

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,5 @@
-WINNER_SONGS = ['Win1.m4a', 'Win2.mp3', 'Win3.mp3', 'Win4.mp3', 'Win5.mp3']
-LOSER_SONGS = ['Lose1.mp3', 'Lose2.mp3', 'Lose3.mp3', 'Lose4.mp3']
+WINNER_SONGS = ['Win1.m4a', 'Win2.mp3', 'Win3.mp3', 'Win4.mp3', 'Win5.mp3', 'Win6.wma', 'Win7.mp3']
+LOSER_SONGS = ['Lose1.mp3', 'Lose2.mp3', 'Lose3.mp3', 'Lose4.mp3', 'Lose5.mp3']
 MAX_SCORE_CAN_BET = 3
 MAX_RECENT_MATCHES = 3
 TOP5 = 5
