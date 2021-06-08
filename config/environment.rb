81c6bd5fc6a6577f13e60b82cd83cd3e466b0173
Thieu Nguyen <thieu@mylong.io>
2021-06-08 13:05:32 +0700

    Change mail setting

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'nus-euro2020.herokuapp.com',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'nus-wc2018.herokuapp.com',
-  :enable_starttls_auto => true
 }
+ActionMailer::Base.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'nus-wc2018.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'nus-euro2020.herokuapp.com' }
 end
