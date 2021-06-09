6d2bd1023a53f875de3789e2fd468259dba586e4
Thieu Nguyen <thieu@mylong.io>
2021-06-09 16:04:42 +0700

    Fix SMTP mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,22 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  domain: 'nus-euro2020.herokuapp.com',
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
-}
+# ActionMailer::Base.smtp_settings = {
+#   address: 'smtp.sendgrid.net',
+#   port: 587,
+#   domain: 'nus-euro2020.herokuapp.com',
+#   user_name: ENV['SENDGRID_USERNAME'],
+#   password: ENV['SENDGRID_PASSWORD'],
+#   authentication: :login,
+#   enable_starttls_auto: true
+# }
 
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'nus-euro2020.herokuapp.com',
+  :authentication => :plain,
+}
 ActionMailer::Base.delivery_method = :smtp
