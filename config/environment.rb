954f48aebbb6c30c03ad0b237d4417e91755e5b5
Thieu Nguyen <thieu.nguyen@oivan.com>
2022-11-15 01:20:04 +0700

    Update smtp setting

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['SMTP_HOST'],
   :user_name      => ENV['SMTP_USERNAME'],
   :password       => ENV['SMTP_PASSWORD'],
-  :domain         => ENV['STMP_DOMAIN'],
-  :authentication => :login,
+  :domain         => ENV['SMTP_DOMAIN'],
+  :authentication => :plain,
   :enable_starttls_auto => true,
   :ssl            => true
 }
